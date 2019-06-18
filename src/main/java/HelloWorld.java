7d7fb3af4a92d082990677e18b030d96ade45848
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2019-06-18 17:18:18 -0400

    develop: HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,2 +1,8 @@
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        String str = " M src/main/java/utils/CommandLineUtils.java";
+        String[] arr = str.split(" ");
+        System.out.println(arr[arr.length - 1]);
+    }
 }
