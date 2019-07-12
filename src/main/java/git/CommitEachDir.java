071ac9eee24c4a7006f0b8d9903d7d590465b5d4
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2019-07-12 15:31:12 -0400

    develop: CommitEachDir

---
+++ src/main/java/git/CommitEachDir.java
@@ -0,0 +1,38 @@
+package git;
+
+import git.utils.CommitUtils;
+import org.apache.commons.lang3.StringUtils;
+import utils.CommandLineUtils;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CommitEachDir {
+
+    public static void main(String[] args) {
+        System.out.println("CommandLineTools::Git::CommitEachDir");
+        commitEachDir();
+    }
+
+    public static void commitEachDir() {
+        String fileStatusStr = CommandLineUtils.run("git status -s");
+        while (StringUtils.isNotBlank(fileStatusStr)) {
+            String[] files = fileStatusStr.split("\n");
+
+            CommitUtils.format(files);
+            Arrays.sort(files, Comparator.reverseOrder());
+
+            String file = files[0];
+
+            System.out.println("");
+            String filePathDir = CommitUtils.getFilePathDir(file);
+            String fileToAdd = StringUtils.isNotBlank(filePathDir) ? filePathDir : file;
+            CommandLineUtils.run("git add " + fileToAdd);
+            CommandLineUtils.run("git commit -m " + fileToAdd);
+            System.out.println("");
+
+            fileStatusStr = CommandLineUtils.run("git status -s");
+        }
+    }
+
+}
