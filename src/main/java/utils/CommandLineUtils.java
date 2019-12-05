6528c76f560dff5da312aa2f5f8e76b8cb30dd10
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2019-12-05 10:22:14 -0500

    develop: CommandLineUtils

--- src/main/java/utils/CommandLineUtils.java
+++ src/main/java/utils/CommandLineUtils.java
@@ -3,10 +3,13 @@ package utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CommandLineUtils {
 
-    public static String run(String command) {
+    public static String runAsString(String command) {
         System.out.println("Running Command: " + command);
         try {
             Process proc = Runtime.getRuntime().exec(command);
@@ -15,7 +18,7 @@ public class CommandLineUtils {
             try {
                 proc.waitFor();
             } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
+                e.printStackTrace();
             }
             StringBuilder sb = new StringBuilder();
             while (read.ready()) {
@@ -25,8 +28,64 @@ public class CommandLineUtils {
             }
             return sb.toString();
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
         return "";
+    }
+
+    public static String run(String command) {
+        return run(convertCommandToArr(command));
+    }
+
+    public static String run(String[] command) {
+        System.out.println("Running Command: " + Arrays.asList(command));
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.command(command);
+            Process proc = processBuilder.start();
+            BufferedReader read = new BufferedReader(new InputStreamReader(
+                    proc.getInputStream()));
+            try {
+                proc.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            StringBuilder sb = new StringBuilder();
+            while (read.ready()) {
+                String line = read.readLine();
+                System.out.println(line);
+                sb.append(line).append("\n");
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static String[] convertCommandToArr(String command) {
+        System.out.println("Input Command: " + command);
+        String[] arr = command.split(" ");
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        boolean buildingParam = false;
+        for (String item : arr) {
+            if (item.startsWith("'")) {
+                buildingParam = true;
+            }
+            if (item.endsWith("'")) {
+                buildingParam = false;
+                sb.append(item);
+                list.add(sb.toString());
+                sb = new StringBuilder();
+            } else if (buildingParam) {
+                sb.append(item).append(" ");
+            } else {
+                list.add(item);
+            }
+        }
+        System.out.println("Output Command: " + list);
+        return list.toArray(new String[list.size()]);
     }
 }
